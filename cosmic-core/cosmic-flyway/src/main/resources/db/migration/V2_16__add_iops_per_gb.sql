9dfeaa01118fe121a1abcf99b0470214534cb64d
Irina Krstic <ikrstic@schubergphilis.com>
2018-02-22 16:31:49 +0100

    wip

---
+++ cosmic-core/cosmic-flyway/src/main/resources/db/migration/V2_16__add_iops_per_gb.sql
@@ -0,0 +1,12 @@
+-- Add miniopspergb, maxiopspergb, highestminiops, highestmaxiops
+ALTER TABLE `disk_offering`
+  ADD COLUMN `min_iops_per_gb` int unsigned,
+  ADD COLUMN `max_iops_per_gb` int unsigned,
+  ADD COLUMN `highest_min_iops` int unsigned,
+  ADD COLUMN `highest_max_iops` int unsigned;
+
+ALTER VIEW `disk_offering_view`
+  ADD COLUMN `min_iops_per_gb` int unsigned,
+  ADD COLUMN `max_iops_per_gb` int unsigned,
+  ADD COLUMN `highest_min_iops` int unsigned,
+  ADD COLUMN `highest_max_iops` int unsigned;
